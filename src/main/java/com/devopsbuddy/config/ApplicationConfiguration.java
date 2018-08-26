529728df95dd7f0ed3a0180be7e599f912bb4db7
vmunguia <mungassey@gmail.com>
2018-08-26 00:36:58 -0500

    Externalized webmaster login and email details

--- src/main/java/com/devopsbuddy/config/ApplicationConfiguration.java
+++ src/main/java/com/devopsbuddy/config/ApplicationConfiguration.java
@@ -2,6 +2,7 @@ package com.devopsbuddy.config;
 
 import org.springframework.boot.orm.jpa.EntityScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
@@ -14,6 +15,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableJpaRepositories(basePackages="com.devopsbuddy.backend.persistance.repositories")
 @EntityScan(basePackages="com.devopsbuddy.backend.persistance.domain.backend")
 @EnableTransactionManagement
+@PropertySource("file:///${user.home}/.devopsbuddy/application-common.properties")
 public class ApplicationConfiguration {
 
 }
